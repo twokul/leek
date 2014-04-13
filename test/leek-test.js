cb59a25485d685dc2740e78c66a4601e0515e0e8
Alex Navasardyan <twokul@users.noreply.github.com>
2014-04-13 18:50:15 -0400

    stuff

---
+++ test/leek-test.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var assert = require('chai').assert,
+    Leek   = require('../lib/leek');
+
+describe('Leek tests', function() {
+  it('Leek exists', function() {
+    assert(Leek);
+  });
+});
