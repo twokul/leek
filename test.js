40a55a224e06cf3e50e7c9266693baf2c84baa43
Alex Navasardyan <twokul@users.noreply.github.com>
2014-04-20 16:00:26 -0400

    Kill inquirer

--- test.js
+++ test.js
@@ -2,8 +2,16 @@ var Leek = require('./lib/leek'),
     leek = new Leek({
       trackingCode: 'UA-49225444-1',
       name:         'ember-cli',
-      version:      'test'
+      version:      'test-some-more'
     });
 
-leek.trackEvent('ember build', 'stuff', 'moar stuff', 'stuff message');
-leek.track('ember build', 'stuff');
+// leek.trackEvent('a', 'b', 'c', 'd');
+// leek.track('ember build', 'stuff ' + Date.now());
+
+leek.trackTiming('rebuild' + Date.now(), 'brocolli', '200ms');
+
+// try {
+  // throw new Error('ZOMG');
+// } catch(e) {
+  // leek.trackError(e.message, true);
+// }
