dbdcce6aa58cad039c236af5f99c29ba6e1933de
Alex Navasardyan <twokul@users.noreply.github.com>
2014-05-27 16:08:34 -0400

    cleans up the tests

--- test/get-versions-test.js
+++ test/get-versions-test.js
@@ -1,6 +1,8 @@
 'use strict';
 
 var assert      = require('chai').assert,
+    ok          = assert.ok,
+    equal       = assert.equal,
     rewire      = require('rewire'),
     getVersions = rewire('../lib/get-versions');
 
@@ -17,67 +19,67 @@ function stubOS(platform, version) {
 
 describe('GetVersions tests', function() {
   it('GetVersions exists', function() {
-    assert.ok(getVersions);
+    ok(getVersions);
   });
 
   it('returns proper version for Mavericks', function() {
     stubOS('darwin', '13.1.0');
-    assert.equal(getVersions().platform, 'OSX Mavericks');
+    equal(getVersions().platform, 'OSX Mavericks');
   });
 
   it('returns proper version for Mountain Lion', function() {
     stubOS('darwin', '12.5.0');
-    assert.equal(getVersions().platform, 'OSX Mountain Lion');
+    equal(getVersions().platform, 'OSX Mountain Lion');
     stubOS('darwin', '12.0.0');
-    assert.equal(getVersions().platform, 'OSX Mountain Lion');
+    equal(getVersions().platform, 'OSX Mountain Lion');
   });
 
   it('returns proper version for Lion', function() {
     stubOS('darwin', '11.4.2');
-    assert.equal(getVersions().platform, 'Mac OSX Lion');
+    equal(getVersions().platform, 'Mac OSX Lion');
     stubOS('darwin', '11.0.0');
-    assert.equal(getVersions().platform, 'Mac OSX Lion');
+    equal(getVersions().platform, 'Mac OSX Lion');
   });
 
   it('returns proper version for Snow Leopard', function() {
     stubOS('darwin', '10.8');
-    assert.equal(getVersions().platform, 'Mac OSX Snow Leopard');
+    equal(getVersions().platform, 'Mac OSX Snow Leopard');
     stubOS('darwin', '10.0');
-    assert.equal(getVersions().platform, 'Mac OSX Snow Leopard');
+    equal(getVersions().platform, 'Mac OSX Snow Leopard');
   });
 
   it('returns proper version for Windows 8.1', function() {
     stubOS('win32', '6.3.9600');
-    assert.equal(getVersions().platform, 'Windows 8.1');
+    equal(getVersions().platform, 'Windows 8.1');
   });
 
   it('returns proper version for Windows 8', function() {
     stubOS('win32', '6.2.9200');
-    assert.equal(getVersions().platform, 'Windows 8');
+    equal(getVersions().platform, 'Windows 8');
   });
 
   it('returns proper version for Windows 7 SP1', function() {
     stubOS('win32', '6.1.7601');
-    assert.equal(getVersions().platform, 'Windows 7 SP1');
+    equal(getVersions().platform, 'Windows 7 SP1');
   });
 
   it('returns proper version for Windows Vista', function() {
     stubOS('win32', '6.0.6000');
-    assert.equal(getVersions().platform, 'Windows Vista');
+    equal(getVersions().platform, 'Windows Vista');
   });
 
   it('returns proper version for Windows Vista SP2', function() {
     stubOS('win32', '6.0.6002');
-    assert.equal(getVersions().platform, 'Windows Vista SP2');
+    equal(getVersions().platform, 'Windows Vista SP2');
   });
 
   it('returns proper version for Windows 7', function() {
     stubOS('win32', '6.1.7600');
-    assert.equal(getVersions().platform, 'Windows 7');
+    equal(getVersions().platform, 'Windows 7');
   });
 
   it('returns raw values if mapping is not found', function() {
     stubOS('raw', 'value');
-    assert.equal(getVersions().platform, 'raw value');
+    equal(getVersions().platform, 'raw value');
   });
 });
--- test/leek-test.js
+++ test/leek-test.js
@@ -2,21 +2,24 @@
 
 var assert = require('chai').assert,
     Leek   = require('../lib/leek'),
+    ok     = assert.ok,
+    equal  = assert.equal,
+    throws = assert.throws,
     leek;
 
 describe('Leek tests', function() {
   it('Leek exists', function() {
-    assert.ok(Leek);
+    ok(Leek);
   });
 
   it('asserts if options are not specified', function() {
-    assert.throws(function() {
+    throws(function() {
       leek = new Leek();
     }, 'You need to specify the options.');
   });
 
   it('asserts if tracking code is not specified', function() {
-    assert.throws(function() {
+    throws(function() {
       leek = new Leek({
         name:         'ember-cli',
         version:      '0.0.23'
@@ -25,7 +28,7 @@ describe('Leek tests', function() {
   });
 
   it('asserts if name is not specified', function() {
-    assert.throws(function() {
+    throws(function() {
       leek = new Leek({
         trackingCode: 'xxxxx',
         version:      '0.0.23'
@@ -41,10 +44,20 @@ describe('Leek tests', function() {
       version:      '0.0.1'
     });
 
-    assert.equal(leek.trackingCode, 'xxxxx', 'tracking code is correct');
-    assert.equal(leek.name, 'ember-cli', 'name is correct');
-    assert.ok(leek.config);
-    assert.equal(leek.version, '0.0.1', 'version is correct');
+    equal(leek.trackingCode, 'xxxxx', 'tracking code is correct');
+    equal(leek.name, 'ember-cli', 'name is correct');
+    ok(leek.config);
+    equal(leek.version, '0.0.1', 'version is correct');
+  });
+
+  it('default version is set if it\'s not specified', function() {
+    leek = new Leek({
+      trackingCode: 'xxxxx',
+      name:         'ember-cli',
+      clientId:     'things'
+    });
+
+    equal(leek.version, '', 'version is an empty string');
   });
 
   it('should have public API methods', function() {
@@ -53,10 +66,10 @@ describe('Leek tests', function() {
       name:         'ember-cli'
     });
 
-    assert.ok(leek.track);
-    assert.ok(leek.trackError);
-    assert.ok(leek.trackTiming);
-    assert.ok(leek.trackEvent);
+    ok(leek.track);
+    ok(leek.trackError);
+    ok(leek.trackTiming);
+    ok(leek.trackEvent);
   });
 
   it('should have private API methods', function() {
@@ -65,6 +78,6 @@ describe('Leek tests', function() {
       name:         'ember-cli'
     });
 
-    assert.ok(leek._getConfigObject);
+    ok(leek._getConfigObject);
   });
 });
